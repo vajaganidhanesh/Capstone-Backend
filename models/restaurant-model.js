09622bc9e6950e69b6863cdefe6463981228c0a8
vajagani dhanesh <vajaganidhanesh@gmail.com>
2022-09-14 06:41:44 +0530

    data bases schema and verification for restaurant

--- models/restaurant-model.js
+++ models/restaurant-model.js
@@ -5,6 +5,19 @@ const restaurantSchema = mongoose.Schema({
         type:String,
         required:true
     },
+    email:{
+        type:String,
+        required:true,
+        unique:[true,"Email Already Exist"]
+    },
+    mobile:{
+        type:Number,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
     address:{
         type:String,
         required:true
--- routes/restaurant.js
+++ routes/restaurant.js
@@ -8,3 +8,44 @@ const userModel = require('../models/user-model')
 
 // for routing the components
 const router = express.Router();
+
+// endpoint for restaurants to get registeration into website
+
+router.post('/signup',(req,res)=>{
+    let restaurant = req.body;
+
+    bcryptjs.genSalt(10,(err,salt)=>{
+
+        if(err===null || err === undefined)
+        {
+
+            bcryptjs.hash(restaurant.password,salt,(err,encPass)=>{
+
+                if(err===null || err === undefined)
+                {
+
+                    restaurant.password = encPass;
+
+                    restaurantModel.create(restaurant)
+                    .then((data)=>{
+
+                        res.status(200).send({success:true,message:"restaurant resgistered successfully"})
+                    })
+                    .catch((err)=>{
+
+                        if(err.code===11000){
+                            res.status(409).send({success:false,message:"restaurant or Username already exits"})
+                        }
+                        else
+                        {
+                            res.status(400).send({success:false,message:err.errors.name.properties.message})
+                        }
+                    })
+
+                }
+
+
+            })
+        }
+    })
+})
