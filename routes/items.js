f9ee575ae1cd63502c510678e8da4f9b263acb57
vajagani dhanesh <vajaganidhanesh@gmail.com>
2022-09-20 09:17:12 +0530

    crud operations are done

--- routes/items.js
+++ routes/items.js
@@ -97,4 +97,61 @@ router.get('/getitems/:adminid',verifytoken,async(req,res)=>{
     })
 })
 
+// to update restaurant      items 
+
+router.put('/update/:id',verifytoken,(req,res)=>{
+
+    let adminid = req.params.id;
+    console.log(adminid);
+
+    let form = new formidable.IncomingForm();
+
+    
+
+    form.parse(req,(err,fields,files)=>{
+
+        if(Object.keys(files).length!==0)
+        {
+            let fileData = fs.readFileSync(files.picture.filepath);
+            let ext = files.picture.originalFilename.split(".")[1].toLowerCase();
+
+            if(ext === "jpg" || ext === "png" || ext === "jpeg")
+            {   
+                let uploadPath = "./foodposts/"+files.picture.newFilename+'.'+ext;
+                let publicPath = "http://localhost:8000/pics/"+files.picture.newFilename+'.'+ext;
+                fields.picture = publicPath;
+                fs.writeFileSync(uploadPath,fileData);
+
+            }   
+        }
+
+        if(Object.keys(fields).length!==0)
+        {
+            itemsModel.findByIdAndUpdate(adminid,fields)
+            .then(async(data)=>{
+                
+                let user = await itemsModel.findById(adminid);
+                res.send({success:true,message:" updated successfully",user});
+            })
+            .catch((err)=>{
+                res.send({message:"Someproblem while updating the item"});
+            })
+        }
+    })
+})
+
+
+router.delete('/deleteitem/:id',(req,res)=>{
+    let id = req.params.id;
+    itemsModel.deleteOne({_id:id})
+    .then((info)=>{
+        res.send({success:true,message:"Comment Deleted successfully",info})
+    })
+    .catch((err)=>{
+        console.log(err);
+        res.send({message:"Some issue while deleting the post"})
+    })
+})
+
+
 module.exports=router
